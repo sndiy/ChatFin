f5ec5b2d5facb4e56024a1f7d6d1220143a8af14
Sndiy <hubaniba@gmail.com>
2026-02-28 10:47:32 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Project-level build.gradle.kts
+plugins {
+    alias(libs.plugins.android.application)     apply false
+    alias(libs.plugins.android.library)         apply false
+    alias(libs.plugins.kotlin.android)          apply false
+    alias(libs.plugins.kotlin.compose)          apply false
+    alias(libs.plugins.kotlin.serialization)    apply false
+    alias(libs.plugins.kotlin.parcelize)        apply false
+    alias(libs.plugins.hilt)                    apply false
+    alias(libs.plugins.ksp)                     apply false
+    alias(libs.plugins.google.services)         apply false
+}
